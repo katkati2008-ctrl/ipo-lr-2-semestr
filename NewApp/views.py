544d417d6f59a0486456d84dbb27b59cb0cb99b0
Kate <Kat.kati2008@gmail.com>
2026-02-08 13:16:41 +0300

    new functions

--- NewApp/views.py
+++ NewApp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def hello_world(request):
+def main(request):
     return render(request,'index.html')
+
+def author(request):
+    content="""
+<title>Об авторе</title>
+<h1>Информация об авторе</h1>
+    <ul>
+        <h2>Автор: Мазалевская Екатерина</h2>
+        <h2>Группа: 89 ТП</h2>
+    </ul>"""
+    return HttpResponse(content)
+
+def about(request):
+    content = """
+    <title>О магазине</title>
+    <h1>Здесь находится основная информация о магазине</h1>
+    <ul>
+       <h2>Данный сайт будет содержать ассортимент,состоящий из товаров для пикника<h2>
+         </ul>"""
+    return HttpResponse(content)
 # Create your views here.
